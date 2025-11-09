ffc50ff73a303d32e9cb4655e5d9aacb442595df
Gabriel Bursi <gabriellbursii@gmail.com>
2025-11-09 14:39:45 -0300

    refactor(swagger): :wrench: add swagger plugin and organize swagger config in a specific file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { configSwagger } from './config/docs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Fincheck API')
-    .setDescription(
-      'O Fincheck é um aplicativo fullstack desenvolvido para ajudar usuários a monitorar suas finanças pessoais de forma fácil e eficiente.\nO objetivo do projeto é fornecer ferramentas que permitam o controle total sobre contas bancárias, investimentos, despesas, receitas e planejamento financeiro.\n\nEste projeto foi desenvolvido como parte do curso JStack e teve como foco a prática de desenvolvimento fullstack moderno, explorando desde o backend com NestJS até o frontend com React.',
-    )
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const documentFactory = () =>
-    SwaggerModule.createDocument(app, config, { autoTagControllers: false });
-  SwaggerModule.setup('openapi', app, documentFactory, {
-    jsonDocumentUrl: 'openapi/json',
-    customSiteTitle: 'Fincheck Swagger',
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api/v1');
+  configSwagger(app);
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
