adb73618df10d4cec0229f4224e06ede24e3100a
Gabriel Bursi <gabriellbursii@gmail.com>
2026-02-22 14:34:40 -0300

    style: :rotating_light: apply lint import order

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
 import { configSwagger } from './config/docs';
 
 async function bootstrap() {
